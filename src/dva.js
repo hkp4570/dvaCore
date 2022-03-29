321b453cdafb2b5b9a67c6e6e7fcad1ba24243c4
hkp <sxhkp163@163.com>
2022-03-29 15:38:40 +0800

    使用dva-loading插件

--- src/dva.js
+++ src/dva.js
@@ -1,16 +1,21 @@
 import {create} from "dva-core";
 import models from './models/index';
+import createLoading from 'dva-loading/dist';
 
 function createApp(opts){
     let app = create(opts);
+    // 使用插件
+    app.use(createLoading({}));
     // 注册model
     models.forEach(model => {
         app.model(model);
     })
     // 启动应用,必须在model注册完成后，在store获取之前
     app.start();
+
     const store = app._store;
     app.getStore = () => store;
+
     app.dispatch = store.dispatch;
     return app;
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {Provider} from "react-redux";
 import App from './app';
-import store from './store';
+import store from './dva';
 
 ReactDom.render(<Provider store={store}><App/></Provider>,document.getElementById('root'));
